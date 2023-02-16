4187af1d09e682c62752b10a637058edb7993cd7
root <root@ip-172-31-7-9.ec2.internal>
2023-02-16 00:03:22 +0000

    Add: Create subnets priv and public

--- network.tf
+++ network.tf
@@ -6,6 +6,29 @@ resource "aws_vpc" "vorx_vpc_prod" {
   }
 }
 
+
+resource "aws_subnet" "vorx-subnet-pub-1a" {
+  vpc_id     = aws_vpc.vorx_vpc_prod.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public-Subnet-1a"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1a" {
+  vpc_id     = aws_vpc.vorx_vpc_prod.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private-Subnet-1a"
+  }
+}
+
+
+
 ##==== Outputs do Nosso Terraform =======##
 
 output "vpc_vorx_id" {
@@ -16,4 +39,6 @@ output "vpc_vorx_arn" {
 value = "aws_vpc.vorx_vpc_prod.arn"
 }
 
-
+output "vorx_pro_subnets" {
+value = "aws_subnet.[*].id"
+}
