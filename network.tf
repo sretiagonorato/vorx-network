63916d10009df2eea463bf9077ebd017bf9adcf7
root <root@ip-172-31-7-9.ec2.internal>
2023-02-16 00:58:23 +0000

    Add: Association route table public

--- network.tf
+++ network.tf
@@ -56,11 +56,30 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vorx_vpc_prod.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
 
+  tags = {
+    Name = "prod-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "pub-rt-1a-associate" {
+  subnet_id      = aws_subnet.vorx-subnet-pub-1a.id
+  route_table_id = aws_route_table.public-rt.id
+}
 
 
-
+resource "aws_route_table_association" "pub-rt-1b-associate" {
+  subnet_id      = aws_subnet.vorx-subnet-pub-1b.id
+  route_table_id = aws_route_table.public-rt.id
+}
 
 
 
